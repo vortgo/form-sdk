836957cf5e3c0a646e7057962ccd49502616d8ca
Yuri Gaidoba <yuri.gaidoba@corp.kismia.com>
2017-02-20 14:20:56 +0200

    SIG2-651 resolved

--- src/scripts/form-validation/components/card-holder.js
+++ src/scripts/form-validation/components/card-holder.js
@@ -4,7 +4,10 @@ import romanize from 'node-romanization';
 export class CardHolder extends Input {
   isValid() {
     let full_name = this.model.get(this.full_name);
-    if (/\s+/.test(full_name)) {
+    if (/^\s+$/.test(full_name)) {
+      return false;
+    }
+    if (!(/\s+/.test(full_name))) {
       return false;
     }
     return /^([\u00c0-\u01ffa-zA-Zа-яА-ЯёЁіІїЇєЄ'\s-]){3,32}$/.test(full_name);
